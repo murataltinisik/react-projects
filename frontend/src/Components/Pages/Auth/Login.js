060c04f926221364d788a2ce05c321ee5348e999
murataltinisik <altinisik.murat40@gmail.com>
2022-08-25 21:43:19 +0300

    REACT SASS UPLOAD

---
+++ frontend/src/Components/Pages/Auth/Login.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+import './style.scss';
+
+class Login extends PureComponent {
+  render() {
+    return <h1>FACEBOOK PROJECT</h1>;
+  }
+}
+
+export default Login;
