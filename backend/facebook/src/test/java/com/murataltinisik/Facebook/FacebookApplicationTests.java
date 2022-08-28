70572febd25f8485c445158c670992543bf680b5
murataltinisik <altinisik.murat40@gmail.com>
2022-08-28 14:30:45 +0300

    Spring Project Changed

---
+++ backend/facebook/src/test/java/com/murataltinisik/Facebook/FacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murataltinisik.Facebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
