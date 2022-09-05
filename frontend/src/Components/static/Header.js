91dfc0f3e52fda407dd2116a4267b8a0b34a1bdd
murataltinisik <altinisik.murat40@gmail.com>
2022-09-05 22:49:15 +0300

    HEADER - MESSENGER

--- frontend/src/Components/static/Header.js
+++ frontend/src/Components/static/Header.js
@@ -6,6 +6,7 @@ import '../assets/Button/button.scss';
 import '../assets/Link/link.scss';
 import '../assets/Display/flex.scss';
 import '../assets/MarginPadding/margin-padding.scss';
+import '../assets/WidthHeight/width-height.scss';
 
 // ? FONT AWESOME
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -27,6 +28,8 @@ import {
   faBullhorn,
   faImagePortrait,
   faBagShopping,
+  faEllipsis,
+  faMaximize,
 } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
@@ -325,7 +328,77 @@ function Header() {
                 </a>
                 <div className={Scss.info}>Messenger</div>
 
-                <div className={Scss.menu}>dsfa</div>
+                <div
+                  className={`${Scss.menu} ${Scss.messengerContainer} width-auto height-auto`}
+                >
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div
+                      className={`${Scss.head} d-flex justify-content-between align-items-center p-1 px-2`}
+                    >
+                      <h4 className={Scss.title}>Sohbetler</h4>
+                    </div>
+
+                    {/* MESSENGER */}
+                    <ul
+                      className={`${Scss.navigate} d-flex justify-content-between align-items-center`}
+                    >
+                      <li className="mr-2">
+                        <FontAwesomeIcon icon={faEllipsis} />
+                      </li>
+                      <li className="mr-2">
+                        <a href="#">
+                          <FontAwesomeIcon icon={faMaximize} />
+                        </a>
+                      </li>
+                      <li className="mr-2">
+                        <FontAwesomeIcon icon={faVideoCamera} />
+                      </li>
+                      <li className="mr-3">
+                        <a href="#">
+                          <FontAwesomeIcon icon={faEdit} />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+
+                  <div>
+                    <div
+                      className={`${Scss.searchMenu} ${Scss.boxShadowNone} p-1`}
+                    >
+                      <div className="search-input">
+                        <FontAwesomeIcon className="icon" icon={faSearch} />
+                        <input type="text" placeholder="Messenger'da Ara" />
+                      </div>
+                    </div>
+
+                    <div className={Scss.body}>
+                      <div className="d-flex justify-content-between align-items-center p-1 px-2">
+                        <div className="d-flex justify-content-start align-items-center">
+                          <div className={Scss.image}>
+                            <img src="https://scontent.fesb10-3.fna.fbcdn.net/v/t1.30497-1/143086968_2856368904622192_1959732218791162458_n.png?stp=dst-png_p100x100&_nc_cat=1&ccb=1-7&_nc_sid=7206a8&_nc_ohc=3H8EZVZrRu0AX8bO26F&_nc_ad=z-m&_nc_cid=1120&_nc_ht=scontent.fesb10-3.fna&oh=00_AT_HfjJbFgU_1SA_Xw0E98ckOKpwDwjTPXeVt1V58u_q2w&oe=633D2A78" />
+                          </div>
+                          <div className="d-flex direction-column ml-1">
+                            <div className={Scss.name}>Kübra Altınışık</div>
+                            <div
+                              className={`${Scss.message} d-flex justify-content-between align-items-center`}
+                            >
+                              <div className={Scss.content}>
+                                Murat Abiciğim Nerdesin
+                              </div>
+                              <div className="elips"></div>
+                              <div className={Scss.createDate}>5 dk</div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className={Scss.isSee}></div>
+                      </div>
+                    </div>
+
+                    <div className={`${Scss.foot} text-center py-1`}>
+                      <a className="primary-link">Messenger'da Tümünü Gör</a>
+                    </div>
+                  </div>
+                </div>
               </li>
 
               <li>
