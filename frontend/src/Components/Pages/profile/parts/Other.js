379e8d317c44ae1486865cc89aedd9b7478a4224
murataltinisik <altinisik.murat40@gmail.com>
2022-09-17 22:27:04 +0300

    POPUP COMPLETED 17.09.2022 22:17

--- frontend/src/Components/Pages/profile/parts/Other.js
+++ frontend/src/Components/Pages/profile/parts/Other.js
@@ -5,10 +5,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faBars,
   faClock,
+  faClose,
   faGear,
   faGlobe,
   faGrip,
   faMessage,
+  faPlus,
   faShare,
   faSliders,
   faThumbsUp,
@@ -19,6 +21,9 @@ import {
 import ShipmentScss from '../../home/parts/styles/component-css/shipment.module.scss';
 import Scss from './styles/other.module.scss';
 import './styles/responsive/responsive.other.scss';
+
+// ? ASSETS
+import '../../../assets/PopUp/popup.scss';
 
 // ? TOP
 import Shipment from '../../home/parts/center-components/shipment';
@@ -47,12 +52,234 @@ class Other extends Component {
     this.state.biographyBtn.style.display = 'block';
   };
 
+  // * POPUP
+  onOpenPopUp = e => {
+    const popups = document.getElementsByClassName('popup');
+    let ariaLabel = '#' + e.target.id;
+    for (let i = 0; i < popups.length; i++) {
+      if (popups[i].getAttribute('aria-label') === ariaLabel) {
+        popups[i].style.display = 'flex';
+      }
+    }
+  };
+
+  onClosePopUp = e => {
+    let isSvg = e.target.tagName;
+    let isPath = e.target.tagName;
+    const popups = document.getElementsByClassName('popup');
+    let ariaLabel;
+
+    if (isSvg) {
+      ariaLabel =
+        e.target.parentElement.parentElement.parentElement.parentElement.getAttribute(
+          'aria-label'
+        );
+    } else if (isPath) {
+      ariaLabel =
+        e.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.getAttribute(
+          'aria-label'
+        );
+    }
+
+    for (let i = 0; i < popups.length; i++) {
+      if (popups[i].getAttribute('aria-label') === ariaLabel) {
+        popups[i].style.display = 'none';
+      }
+    }
+  };
+
   render() {
     return (
       <div
         id={Scss.OtherComponents}
         className="otherComponentsResp d-flex justify-content-between align-items-start"
       >
+        {/* DETAIL EDITING */}
+        <div
+          className="popup"
+          onMouseLeave={() =>
+            (document.getElementsByClassName('popup')[0].style.display = 'none')
+          }
+          aria-label="#detailContainer"
+        >
+          <div className="container detail-container">
+            {/* HEAD */}
+            <div className="head">
+              <div className="title">Detayları Düzenle</div>
+              <div className="close" onClick={this.onClosePopUp}>
+                <FontAwesomeIcon icon={faClose} />
+              </div>
+            </div>
+
+            {/* BODY */}
+            <div className="body">
+              {/* TITLE & DESCRIPTION */}
+              <div>
+                <div className="title">Künyeni Özelleştir</div>
+                <div className="description">
+                  Seçtiğin detaylar herkese açık olacak.
+                </div>
+              </div>
+
+              {/* CONTENT */}
+              <div className="content">
+                <ul>
+                  <li className="d-flex direction-column">
+                    <span>İş</span>
+                    <a href="#">
+                      <FontAwesomeIcon icon={faPlus} />
+                      <span>Bir İşyeri Ekle</span>
+                    </a>
+                  </li>
+
+                  <li className="d-flex direction-column">
+                    <span>Eğitim</span>
+                    <div className="d-flex direction-column align-items-start justify-content-center">
+                      <a href="#">
+                        <FontAwesomeIcon icon={faPlus} />
+                        <span>Lise Ekle</span>
+                      </a>
+                      <a href="#">
+                        <FontAwesomeIcon icon={faPlus} />
+                        <span>Üniversite Ekle</span>
+                      </a>
+                    </div>
+                  </li>
+
+                  <li className="d-flex direction-column">
+                    <span>Yaşadığı Şehir</span>
+                    <a href="#">
+                      <FontAwesomeIcon icon={faPlus} />
+                      <span>Yaşadığı şehri ekle</span>
+                    </a>
+                  </li>
+
+                  <li className="d-flex direction-column">
+                    <span>Memleket</span>
+                    <a href="#">
+                      <FontAwesomeIcon icon={faPlus} />
+                      <span>Memleket Ekle</span>
+                    </a>
+                  </li>
+
+                  <li className="d-flex direction-column">
+                    <span>İlişki</span>
+                    <a href="#">
+                      <FontAwesomeIcon icon={faPlus} />
+                      <span>İlişki durumunu Ekle</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            {/* FOOT */}
+            <div className="foot">
+              <a className="primary-link">Bilgilerini Güncelle</a>
+              <div className="group">
+                <button className="secondary-btn">İptal</button>
+                <button className="primary-btn">Kaydet</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* HOBIES ADD */}
+        <div
+          className="popup"
+          onMouseLeave={() =>
+            (document.getElementsByClassName('popup')[1].style.display = 'none')
+          }
+          aria-label="#hobiesContainer"
+        >
+          <div className="container hobies-container">
+            {/* HEAD */}
+            <div className="head">
+              <div className="title">
+                <p>Hobi Ekle</p>
+                <p className="description">
+                  Neler yapmayı seviyorsun? Aşağıdaki popüler hobilerden seçim
+                  yap veya başka hobiler ekle.
+                </p>
+              </div>
+              <div className="close" onClick={this.onClosePopUp}>
+                <FontAwesomeIcon icon={faClose} />
+              </div>
+            </div>
+
+            {/* BODY */}
+            <div className="body">
+              {/* TITLE & DESCRIPTION */}
+              <div>
+                <div className="title text-center">TAVSİYE EDILEN HOBİLER</div>
+              </div>
+
+              {/* CONTENT */}
+              <div className="content">
+                <ul>
+                  <li>Kitap Okuma</li>
+                  <li className="selection">Müzik Dinleme</li>
+                  <li>Seyahat</li>
+                  <li className="selection">Film İzleme</li>
+                  <li>Doğa Yürüyüşü</li>
+                  <li>Alışveriş</li>
+                  <li className="selection">Yürüyüş</li>
+                  <li>Doğa Fotoğrafçılığı</li>
+                  <li>Yemek Pişirme</li>
+                  <li>Araba Sürme</li>
+                  <li className="search">Başka Ara</li>
+                </ul>
+              </div>
+            </div>
+
+            {/* FOOT */}
+            <div className="foot">
+              <a
+                href="javascript:void(0)"
+                className="dark-link text-decoration-none"
+              >
+                <FontAwesomeIcon icon={faGlobe} />
+                <span className="ml-1">Hobiler Herkese Açıktır</span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* EDIT FEATURED */}
+        <div
+          className="popup"
+          onMouseLeave={() =>
+            (document.getElementsByClassName('popup')[2].style.display = 'none')
+          }
+          aria-label="#editFeaturedContainer"
+        >
+          <div className="container featured-container">
+            {/* HEAD */}
+            <div className="head">
+              <div className="title">
+                <p>Öne Çıkanları Düzenle</p>
+              </div>
+              <div className="close" onClick={this.onClosePopUp}>
+                <FontAwesomeIcon icon={faClose} />
+              </div>
+            </div>
+
+            {/* BODY */}
+            <div className="body">
+              {/* TITLE & DESCRIPTION */}
+              <div>
+                <img src="https://static.xx.fbcdn.net/rsrc.php/v3/yN/r/gL1slwup025.png" />
+              </div>
+
+              <div className="content">
+                Tüm arkadaşlarının görmesi için en sevdiğin fotoğrafları ve
+                hikayeleri burada öne çıkar.
+                <button className="facebook-btn mt-1">Yeni Ekle</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
         {/* LEFT */}
         <div className={`${Scss.left} leftResp`}>
           <div className={`${Scss.tags} card-2`}>
@@ -91,11 +318,27 @@ class Other extends Component {
                   </div>
                 </div>
               </li>
-              <li className="secondary-btn text-center mt-1 p-05">
+
+              <li
+                className="secondary-btn text-center mt-1 p-05"
+                id="detailContainer"
+                onClick={this.onOpenPopUp}
+              >
                 Detayları Düzenle
               </li>
-              <li className="secondary-btn text-center mt-1 p-05">Hobi Ekle</li>
-              <li className="secondary-btn text-center mt-1 p-05">
+
+              <li
+                className="secondary-btn text-center mt-1 p-05"
+                id="hobiesContainer"
+                onClick={this.onOpenPopUp}
+              >
+                Hobi Ekle
+              </li>
+              <li
+                className="secondary-btn text-center mt-1 p-05"
+                id="editFeaturedContainer"
+                onClick={this.onOpenPopUp}
+              >
                 Öne Çıkarma Ekle
               </li>
             </ul>
